3a8d85363b457f4343d48aa770c1747ea143b8bb
overZaOzAo <104659177+overZaOzAo@users.noreply.github.com>
2022-04-29 23:51:20 -0300

    Comando Calvos-Teste 29/04

---
+++ calvotinho/comandos/calvos.py
@@ -0,0 +1,12 @@
+from random import seed     
+from random import random
+from random import randint
+
+
+a = (randint(0,100))
+
+if(a<1):
+    print(f'Olha só, você não sera calvo!')
+
+else:
+    print(f'Chance de voce ser calvo e de {a}%.')
